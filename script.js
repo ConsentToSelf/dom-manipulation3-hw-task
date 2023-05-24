1ca1d058860efcf3beeb9f266452b95ea646ec33
Султан <carameleclipsimus@gmail.com>
2023-05-24 22:32:16 +0600

    feat: second task of dom-manipulation3 is done

--- script.js
+++ script.js
@@ -18,21 +18,36 @@
 Вручную номер li не ставить оно должно подставляться в зависимости от кол-ва лишек в списке.
 */
 
-const list = document.querySelector("ul");
-const filteredList = [...list.children];
-const current = list.children.length;
-function getNewDom(args) {
-  const fragment = document.createDocumentFragment();
-  let child = document.createElement("a");
-  args.forEach((element, index) => {
-    element = document.createElement("li");
-    child = document.createElement("a");
-    child.href = "#";
-    child.textContent = `Link${index + current + 1}`;
-    element.appendChild(child);
-    element.classList.add("new-item");
-    fragment.appendChild(element);
-  });
-  list.appendChild(fragment);
-}
-getNewDom(filteredList);
+let list = document.querySelector("ul");
+// const filteredList = [...list.children];
+// const current = list.children.length;
+// function getNewDom(args) {
+//   const fragment = document.createDocumentFragment();
+//   let child = document.createElement("a");
+//   args.forEach((element, index) => {
+//     element = document.createElement("li");
+//     child = document.createElement("a");
+//     child.href = "#";
+//     child.textContent = `Link${index + current + 1}`;
+//     element.appendChild(child);
+//     element.classList.add("new-item");
+//     fragment.appendChild(element);
+//   });
+//   list.appendChild(fragment);
+// }
+// getNewDom(filteredList);
+
+// second
+
+/*
+В каждую ссылку, которая находятся внутри списка ul добавить по тегу strong (в каждую ссылку один - strong).
+*/
+
+// const fragment = document.createDocumentFragment();
+const link = document.querySelectorAll("a");
+
+link.forEach((elem) => {
+  const strong = document.createElement("strong");
+  strong.textContent = "Hi guys!";
+  elem.appendChild(strong);
+});
